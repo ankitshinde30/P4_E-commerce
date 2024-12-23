eb90e98f2f6bcd7b2def20266de09a72d37b1abf
Ankitshinde30 <ankit.shinde2611@gmail.com>
2024-12-23 21:16:50 +0530

    "Add 'New Arrivals' section to homepage"

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./NewCollections.css";
+import new_collection from '../Assets/new_collections';
+
 
 const NewCollections = () => {
 
@@ -8,7 +10,10 @@ const NewCollections = () => {
             <h1>New Collections</h1>
             <hr />
             <div className="collections">
-                
+                {new_collection.map((item,id)=>{
+                     return <item key={id} image={item.image} name={item.name} new_price={item.new_price} old_price={item.old_price} />
+                })
+                };
             </div>
         </div>
     );
