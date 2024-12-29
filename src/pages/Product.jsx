e82c1cf95f0fbc8fc06bc1f64517dc5ac1425a06
Ankitshinde30 <ankit.shinde2611@gmail.com>
2024-12-29 20:57:56 +0530

    added the product pages and content

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 
 
 const Product = () => {
+    const {all_products} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_products.find(product => product.id === parseInt(productId));
     return (
         <div>
-           
+           <Breadcrumb product={product} />
         </div>
     );
 }
